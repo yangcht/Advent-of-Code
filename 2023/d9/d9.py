97da04bc2b30e26398bec094270bca733542c8fb
Chentao Yang <yangcht@gmail.com>
2023-12-10 11:09:51 +0100

    functionized the code

--- 2023/d9/d9.py
+++ 2023/d9/d9.py
@@ -5,26 +5,21 @@ with open('./d9_input.txt') as f:
     line = [lines.split() for lines in [lines.rstrip() for lines in f]]
     num_line = [[int(item) for item in sublist] for sublist in line]
 
+def extrapolate_and_sum(num_list, reverse=False):
+    sum_extrapolated_values = 0
+
+    for line in num_list:
+        if reverse:
+            line = line[::-1]
+        interpolator = BarycentricInterpolator(np.arange(len(line)), np.array(line))
+        x_values = np.linspace(0, len(line), len(line))
+        y_values = interpolator(x_values)
+        sum_extrapolated_values += y_values[-1]
+
+    return round(sum_extrapolated_values)
+
 #### Part 1
-extrapolated_values = []
-
-for i, line in enumerate(num_line):
-
-    interpolator = BarycentricInterpolator(np.arange(len(line)), np.array(line))
-    x_values = np.linspace(0, len(line), len(line))
-    y_values = interpolator(x_values)
-    extrapolated_values.append(y_values[-1])
-
-print("Sum of extrapolated values:", round(sum(extrapolated_values)))
-
+print(extrapolate_and_sum(num_line))
 
 #### Part 2
-extrapolated_values_reverse = []
-
-for i, line in enumerate(num_line):
-    interpolator = BarycentricInterpolator(np.arange(len(line)), np.array(line)[::-1])
-    x_values = np.linspace(0, len(line), len(line))
-    y_values = interpolator(x_values)
-    extrapolated_values_reverse.append(y_values[-1])
-
-print("Sum of extrapolated values:", round(sum(extrapolated_values_reverse)))
+print(extrapolate_and_sum(num_line, reverse=True))
