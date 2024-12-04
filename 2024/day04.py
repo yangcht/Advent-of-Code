e352ca7310a5882ba97dbf286bf8b3131869845a
Chentao Yang <yangcht@gmail.com>
2024-12-04 21:18:48 +0100

    Day 04 Part 2

--- 2024/day04.py
+++ 2024/day04.py
@@ -4,14 +4,13 @@ import numpy as np
 def count_xmas(grid):
     target = "XMAS"
     directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, -1), (1, -1), (-1, 1)]
-    t_len = len(target)
 
     def valid(r, c, dr, dc):
         return all(
             0 <= r + k * dr < grid.shape[0] and
             0 <= c + k * dc < grid.shape[1] and
             grid[r + k * dr, c + k * dc] == target[k]
-            for k in range(t_len)
+            for k in range(len(target))
         )
 
     return sum(
@@ -21,8 +20,29 @@ def count_xmas(grid):
         for dr, dc in directions
     )
 
+# PART 2: counting the incidents of the corss MAS
+def count_x_mas(grid):
+
+    grid = np.array([list(row) for row in grid])
+
+    center_A = grid[1:-1, 1:-1] == 'A'
+
+    tl = grid[:-2, :-2]
+    tr = grid[:-2, 2:]
+    bl = grid[2:, :-2]
+    br = grid[2:, 2:]
+
+    filter1 = (tl == 'M') & (tr == 'M') & (bl == 'S') & (br == 'S')
+    filter2 = (tl == 'M') & (tr == 'S') & (bl == 'M') & (br == 'S')
+    filter3 = (tl == 'S') & (tr == 'M') & (bl == 'S') & (br == 'M')
+    filter4 = (tl == 'S') & (tr == 'S') & (bl == 'M') & (br == 'M')
+
+    total_x_mas = np.sum(center_A & (filter1 | filter2 | filter3 | filter4))
+
+    return total_x_mas
+
 if __name__ == "__main__":
     matrix = np.genfromtxt("./inputs/day04_1.txt", dtype=str, delimiter="\n")
     grid = np.array([list(row) for row in matrix])
-    nr_xmas = count_xmas(grid)
-    print(f"Q1 counts: {nr_xmas}")
+    print(f"Q1 counts: {count_xmas(grid)}")
+    print(f"Q2 counts: {count_x_mas(grid)}")
