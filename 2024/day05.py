91619bb1d22b805a6fa00632dcf7c4c3ab6e56f4
Chentao Yang <yangcht@gmail.com>
2024-12-05 21:31:06 +0100

    Day 5, Part 2

--- 2024/day05.py
+++ 2024/day05.py
@@ -11,10 +11,10 @@ def check_rules_for_page(page, rules):
     for rule in rules:
 
         match = [page.index(r) for r in rule if r in page]
-        
+
         if not ((len(match) == 2 and match[1] > match[0]) or len(match) <= 1):
             return False
-            
+
     return True 
 
 middle_page = [
@@ -23,4 +23,26 @@ middle_page = [
     if check_rules_for_page(page, rules)
 ]
 
+# PART II: Reorder pages according to the rules:
+ordered_page = []
+
+def order_page_by_rules(page, rules):
+    violate_rule = True
+    while violate_rule:
+        violate_rule = False
+        for rule in rules:
+            match = [page.index(r) for r in rule if r in page]
+            if len(match) == 2 and match[1] < match[0]:
+                page[match[0]], page[match[1]] = page[match[1]], page[match[0]]
+                violate_rule = True
+
+    return page
+
+middle_ordered_page = [
+    order_page_by_rules(page[:], rules)[len(page) // 2]
+    for page in pages
+    if not check_rules_for_page(page, rules)
+]
+
 print(f'Q1 sum: {sum(middle_page)}')
+print(f'Q2 sum: {sum(middle_ordered_page)}')
