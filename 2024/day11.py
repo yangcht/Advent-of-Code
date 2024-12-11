36153b1d58e8880a61834b206780cb38bc524fb3
Chentao Yang <yangcht@gmail.com>
2024-12-11 19:36:49 +0100

    Optmize Day 11

--- 2024/day11.py
+++ 2024/day11.py
@@ -1,59 +1,55 @@
-import sys
-
-def r0(n):
+def r1(n):
     return [1]
 
-def rs(n):
+def r2(n):
     s = str(n)
     h = len(s) // 2
-    l = int(s[:h])
-    r = int(s[h:])
+    l = int(s[:h]) if h > 0 else 0
+    r = int(s[h:]) if h < len(s) else 0
     return [l, r]
 
-def rm(n):
+def r3(n):
     return [n * 2024]
 
-def ar(n):
+rule_cache = {}
+def rules(n):
+    if n in rule_cache:
+        return rule_cache[n]
     if n == 0:
-        return r0(n)
-    d = len(str(n))
-    if d % 2 == 0:
-        return rs(n)
-    return rm(n)
+        result = r1(n)
+    elif len(str(n)) % 2 == 0:
+        result = r2(n)
+    else:
+        result = r3(n)
+    rule_cache[n] = result
+    return result
 
-def ri(fp):
+def read_input(fp):
     with open(fp, 'r') as f:
-        c = f.read()
-    return list(map(int, c.strip().split()))
+        return list(map(int, f.read().strip().split()))
 
 def main(num):
-    fp = './inputs/day11_1.txt'
-    is_ = ri(fp)
+    ini_states = read_input('./inputs/day11_1.txt')
+    to_blink = num
 
-    tb = num
-    memo = {}
+    current_cnt = {}
+    for s in ini_states:
+        current_cnt[s] = current_cnt.get(s, 0) + 1
 
-    def cs(n, b):
-        if b == 0:
-            return 1
-        k = (n, b)
-        if k in memo:
-            return memo[k]
-        ns = ar(n)
-        t = 0
-        for nn in ns:
-            t += cs(nn, b - 1)
-        memo[k] = t
-        return t
+    for step in range(1, to_blink + 1):
+        next_cnts = {}
+        for n, cnt in current_cnt.items():
+            for nn in rules(n):
+                next_cnts[nn] = next_cnts.get(nn, 0) + cnt
+        current_cnt = next_cnts
 
-    ts = 0
-    for s in is_:
-        ts += cs(s, tb)
-
-    print(f"{ts}")
+    total = sum(current_cnt.values())
+    print(f"{total}")
 
 if __name__ == "__main__":
     print("Q1:", end=" ")
     main(25)
     print("Q2:", end=" ")
     main(75)
+    print("blink 10000 times:", end=" ")
+    main(10000)
