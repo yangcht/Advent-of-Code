8ebca31277c7d5a42d39cb2da052a84d866074a5
Chentao Yang <yangcht@gmail.com>
2024-12-14 18:45:16 +0100

    Day 14 speed improve by down-sampling before calculating the entropy

--- 2024/day14.py
+++ 2024/day14.py
@@ -13,41 +13,40 @@ def simulate(pos, vel, w, h, sec):
 
 def count_quads(pos, w, h):
     midx, midy = w // 2, h // 2
-    valid = pos[(pos[:,0] != midx) & (pos[:,1] != midy)]
-    q1 = np.sum((valid[:,0] < midx) & (valid[:,1] < midy))
-    q2 = np.sum((valid[:,0] > midx) & (valid[:,1] < midy))
-    q3 = np.sum((valid[:,0] < midx) & (valid[:,1] > midy))
-    q4 = np.sum((valid[:,0] > midx) & (valid[:,1] > midy))
-    return q1, q2, q3, q4
+    x, y = pos[:, 0], pos[:, 1]
+    return (
+        np.sum((x < midx) & (y < midy) & (x != midx) & (y != midy)),
+        np.sum((x > midx) & (y < midy) & (x != midx) & (y != midy)),
+        np.sum((x < midx) & (y > midy) & (x != midx) & (y != midy)),
+        np.sum((x > midx) & (y > midy) & (x != midx) & (y != midy)),
+    )
 
-def safety_factor(quads):
-    return np.prod(quads)
-
-def create_grid(pos, w, h):
-    grid = np.zeros((h, w), dtype=int)
-    np.add.at(grid, (pos[:,1], pos[:,0]), 1)
-    return grid
-
-def calc_entropy(grid):
+def calc_entropy(pos, w, h):
+    pos_down = pos // 2
+    grid = np.zeros(((h + 1) // 2, (w + 1) // 2), dtype=int)
+    np.add.at(grid, (pos_down[:, 1], pos_down[:, 0]), 1)
     counts = np.bincount(grid.flatten())
-    probs = counts[counts > 0] / counts.sum()
+    counts = counts[counts > 0]
+    probs = counts / counts.sum()
     return -np.sum(probs * np.log2(probs))
 
 def find_pattern_sec(pos, vel, w, h, max_sec=10000):
-    min_sec, min_ent = None, float('inf')
+    min_sec, min_ent, curr_pos = None, float('inf'), pos.copy()
     for sec in range(1, max_sec + 1):
-        curr_pos = simulate(pos, vel, w, h, sec)
-        ent = calc_entropy(create_grid(curr_pos, w, h))
+        curr_pos = (curr_pos + vel) % [w, h]
+        ent = calc_entropy(curr_pos, w, h)
         if ent < min_ent:
-            min_ent, min_sec = ent, sec
+            min_sec, min_ent = sec, ent
     return min_sec
 
 def main():
     pos, vel = parse('./inputs/day14_1.txt')
     w, h = 101, 103
     final_pos = simulate(pos, vel, w, h, 100)
-    quads = count_quads(final_pos, w, h)
-    print(f"Q1 safe factor: {safety_factor(quads)}")
+
+    #PART 1: calculate the numbers in the quardrants
+    print(f"Q1 safe factor: {np.prod(count_quads(final_pos, w, h))}")
+    #PART 2: searching the patterns
     print(f"Q2 easter-egg second: {find_pattern_sec(pos, vel, w, h)}")
 
 if __name__ == "__main__":
